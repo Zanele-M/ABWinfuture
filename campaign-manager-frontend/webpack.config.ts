e6697b462ad5d4a870505360592ccf5fd9f92bc9
Zanele-M <zsp2220696.sibanyoni@gmail.com>
2023-06-27 22:28:06 +0200

    Properly added webpack for the frontend

--- campaign-manager-frontend/webpack.config.ts
+++ campaign-manager-frontend/webpack.config.ts
@@ -1,16 +1,49 @@
-import webpack from 'webpack';
-import WebpackDevServer from 'webpack-dev-server';
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack, { Configuration as WebpackConfiguration } from 'webpack';
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
 
-interface Configuration extends webpack.Configuration {
-  devServer?: WebpackDevServer.Configuration;
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
 }
 
 const config: Configuration = {
-  // ...other webpack configuration...
-  devServer: {
-    allowedHosts: 'all',
-    // ...other devServer configuration...
+  mode: 'development', // Add this line
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
   },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ['ts-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: "./public/favicon.ico"
+    }),
+  ],
 };
 
 export default config;
