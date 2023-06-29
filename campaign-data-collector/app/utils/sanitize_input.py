5256bc52e098ed3fd2cbb6e783bc6da1efd223f6
Zanele-M <zsp2220696.sibanyoni@gmail.com>
2023-06-29 19:38:27 +0200

    mend

--- campaign-data-collector/app/utils/sanitize_input.py
+++ campaign-data-collector/app/utils/sanitize_input.py
@@ -6,23 +6,25 @@ logger = logging.getLogger(__name__)
 def sanitize_input(data):
     try:
         # Ensure all required fields are present
-        required_fields = ["assigned_id", "interaction_type", "is_control"]
+        required_fields = ["assignedId", "interactionType", "isControl"]
         if not all(field in data for field in required_fields):
             return None, "Missing one or more required fields"
 
-        # Check if assigned_id field exists and is not None
-        if "assigned_id" not in data or data["assigned_id"] is None:
-            return None, "Field 'assigned_id' is missing or None"
+        # Check if assignedId field exists and is not None
+        if "assignedId" not in data or data["assignedId"] is None:
+            return None, "Field 'assignedId' is missing or None"
 
         # Ensure fields are of correct type
-        if not isinstance(data["assigned_id"], int):
-            return None, "Invalid type for field 'assigned_id'"
-        if not isinstance(data["interaction_type"], str) or data["interaction_type"] not in ["views", "clicks"]:
-            return None, "Invalid type or value for field 'interaction_type'"
-        if not isinstance(data["is_control"], bool):
-            return None, "Invalid type for field 'is_control'"
+        if not isinstance(data["assignedId"], int):
+            return None, "Invalid type for field 'assignedId'"
+        if not isinstance(data["interactionType"], str) or data["interactionType"] not in ["views", "clicks"]:
+            return None, "Invalid type or value for field 'interactionType'"
+        if not isinstance(data["isControl"], bool):
+            return None, "Invalid type for field 'isControl'"
 
-        sanitized_data = data
+        sanitized_data = {'assigne_id' : data['assignedId'],
+                          'is_control' : data['isControl'],
+                          'interaction_type' : data['interactionType']}
         return sanitized_data, None
     except Exception as e:
         extra_data = {"data": data}
