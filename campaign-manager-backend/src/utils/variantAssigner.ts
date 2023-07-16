47789328c600ea8106936a6ecedccbca5548206b
Zanele <zsp220696.sibanyoni@gmail.com>
2023-07-16 19:28:54 +0000

    Added tompson sampling for assigning the variant

--- campaign-manager-backend/src/utils/variantAssigner.ts
+++ campaign-manager-backend/src/utils/variantAssigner.ts
@@ -2,29 +2,12 @@ import { Campaign, CampaignCookie, Variants, Controls } from "../types";
 import { Aggregates } from "./types";
 import { Database } from "../database/database";
 import { rollbar } from '../index'; // adjust the path according to your project structure
-
+import jStat from 'jstat'; // You'll need to install this library
 
 function assignRandomVariant(campaign: Campaign, control: Controls, variants: Variants[]): CampaignCookie {
-  // If there are no variants, assign the control variant
-  if (!variants || variants.length === 0) {
-    console.log('No variants found, assigning control variant');
-    return {
-      campaignId: campaign.id,
-      assignedIdentifier: control.identifier,
-      controlIdentifier: control.identifier,
-      assignedId: control.id,
-      type: campaign.type,
-      isControl: true,
-    };
-  }
-
-  // Otherwise, choose a random variant (including the control variant)
-  const allVariants = [
-    control,
-    ...variants
-  ];
+  const allVariants = [control, ...variants];
   const assigned = allVariants[Math.floor(Math.random() * allVariants.length)];
-  console.log(`Assigned variant: ${assigned.name}`);
+  rollbar.log(`Assigned variant: ${assigned.name}`);
   return {
     campaignId: campaign.id,
     assignedIdentifier: assigned.identifier,
@@ -35,85 +18,59 @@ function assignRandomVariant(campaign: Campaign, control: Controls, variants: Va
   };
 }
 
-
-function assignVariantBasedOnCTR(campaign: Campaign, control: Controls, variants: Variants[], controlAggregates: Aggregates, variantAggregates: Aggregates[]): CampaignCookie {
-  // Calculate the total clicks and views for the control
-  const controlTotalClicks = controlAggregates.totalClicks;
-  const controlTotalViews = controlAggregates.totalViews;
-  const controlCTR = controlTotalViews === 0 ? 0 : controlTotalClicks / controlTotalViews;
-
-  // Calculate the total clicks and views for each variant and calculate the CTR
-  const variantCTRs = variantAggregates.map(aggregate => {
-    const totalClicks = aggregate.totalClicks;
-    const totalViews = aggregate.totalViews;
-    return totalViews === 0 ? 0 : totalClicks / totalViews;
-  });
-
-  // Combine the control CTR and variant CTRs
-  const allCTRs = [controlCTR, ...variantCTRs];
-
-  // Calculate the total CTR
-  const totalCTR = allCTRs.reduce((sum, ctr) => sum + ctr, 0);
-
-  // Generate a random number between 0 and the total CTR
-  const randomNumber = Math.random() * totalCTR;
-
-  // Find the assigned variant/control based on the generated random number and the CTR values
-  let cumulativeCTR = 0;
-  for (let i = 0; i < allCTRs.length; i++) {
-    cumulativeCTR += allCTRs[i];
-    if (randomNumber <= cumulativeCTR) {
-      const assigned = i === 0 ? control : variants[i - 1];
-      if (assigned) {
-        console.log(`Assigned variant/control: ${assigned.name} with CTR: ${allCTRs[i]}`);
-        return {
-          campaignId: campaign.id,
-          assignedIdentifier: assigned.identifier,
-          controlIdentifier: control.identifier,
-          assignedId: assigned.id,
-          type: campaign.type,
-          isControl: i === 0,
-        };
-      }
-    }
-  }
-
-  rollbar.error('No variant was selected');
-  console.error('No variant was selected');
-  throw new Error('No variant was selected');
-}
-
-export async function assignVariantToUser(campaign: Campaign, control: Controls, variants: Variants[], db: Database): Promise<CampaignCookie> {
-  // Fetch the confidence interval upper value for the campaign
-  const confidenceIntervalUpper = await db.getConfidenceIntervalUpper(campaign.id);
-
-  // If the confidence interval is null or less than or equal to 60, assign a random variant
-  if (confidenceIntervalUpper === null || confidenceIntervalUpper <= 60) {
-    rollbar.log('Confidence interval is null or less than or equal to 60, assigning a random variant');
-    console.log('Confidence interval is null or less than or equal to 60, assigning a random variant');
-    return assignRandomVariant(campaign, control, variants);
-  }
-
-  // If the confidence interval is greater than 60, fetch the aggregates and assign based on CTR
-  if (confidenceIntervalUpper > 60) {
-    // Get the aggregates for the control and variants from the aggregate table
+async function assignVariantBasedOnThompsonSampling(campaign: Campaign, control: Controls, variants: Variants[], db: Database): Promise<CampaignCookie> {
+  try {
     const controlAggregates = await db.getAggregateResults(control.id);
     const variantAggregates = await Promise.all(variants.map(variant => db.getAggregateResults(variant.id)));
 
-    // Check if the aggregates for the control and variants are empty
     if (controlAggregates === null || variantAggregates.some(aggregate => aggregate === null || aggregate === undefined)) {    
-      rollbar.log('No aggregates found, assigning control variant or a random variant');    
-      console.log('No aggregates found, assigning control variant or a random variant');
+      rollbar.log('No aggregates found, assigning control variant or a random variant');
       return assignRandomVariant(campaign, control, variants);
     }
 
-    rollbar.log('Aggregates found and confidence interval is above 60% assigning based on CTR');
-    console.log('Aggregates found and confidence interval is above 60% assigning based on CTR');
-    return assignVariantBasedOnCTR(campaign, control, variants, controlAggregates, variantAggregates);
+    const allClicksAndViews = [
+      { totalClicks: controlAggregates.totalClicks, totalViews: controlAggregates.totalViews },
+      ...variantAggregates.map(aggregate => ({ totalClicks: aggregate.totalClicks, totalViews: aggregate.totalViews })),
+    ];
+
+    const sampledCTRs = allClicksAndViews.map(({ totalClicks, totalViews }) => {
+      const alpha = totalClicks + 1;
+      const beta = totalViews - totalClicks + 1;
+      return jStat.beta.sample(alpha, beta);
+    });
+
+    const maxSampledCTR = Math.max(...sampledCTRs);
+    const assignedIndex = sampledCTRs.indexOf(maxSampledCTR);
+    const assigned = assignedIndex === 0 ? control : variants[assignedIndex - 1];
+
+    rollbar.log(`Assigned variant/control: ${assigned.name} with sampled CTR: ${maxSampledCTR}`);
+    return {
+      campaignId: campaign.id,
+      assignedIdentifier: assigned.identifier,
+      controlIdentifier: control.identifier,
+      assignedId: assigned.id,
+      type: campaign.type,
+      isControl: assignedIndex === 0,
+    };
+  } catch (error) {
+    rollbar.error(`Failed to assign variant based on Thompson Sampling: ${error}`);
+    throw error;
+  }
+}
+
+export async function assignVariantToUser(campaign: Campaign, control: Controls, variants: Variants[], db: Database): Promise<CampaignCookie> {
+  const confidenceIntervalUpper = await db.getConfidenceIntervalUpper(campaign.id);
+
+  if (confidenceIntervalUpper === null || confidenceIntervalUpper <= 60) {
+    rollbar.log('Confidence interval is null or less than or equal to 60, assigning a random variant');
+    return assignRandomVariant(campaign, control, variants);
   }
 
-  // Return a random variant as a default if other conditions are not met
+  if (confidenceIntervalUpper > 60) {
+    rollbar.log('Confidence interval is above 60%, assigning based on Thompson Sampling');
+    return assignVariantBasedOnThompsonSampling(campaign, control, variants, db);
+  }
+
   rollbar.log('No specific condition was met, assigning a random variant');
-  console.log('No specific condition was met, assigning a random variant');
   return assignRandomVariant(campaign, control, variants);
 }
